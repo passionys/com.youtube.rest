67c6941c45ea905aca7e95a219b6b392eb20ccaf
passion <youngshinkim94@gmail.com>
2014-09-18 11:50:30 -0400

    finished ep2

---
+++ src/com/youtube/rest/status/V1_status.java
@@ -0,0 +1,22 @@
+package com.youtube.rest.status;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+@Path("/v1/status")
+public class V1_status {
+
+	private static final String version="00.01.00";
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String returnTitle(){
+		return "<p>Java Web Service</p>";
+	}
+	
+	@Path("/version")
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String returnVersion(){
+		return "<p>Version:</p>" + version;
+	}
+}
